a492eb85e2b61967287f05fc1e03d25a9ff48aa4
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2020-04-25 00:41:29 -0400

    removed printing

--- mlexpt/data/dataload.py
+++ mlexpt/data/dataload.py
@@ -41,7 +41,6 @@ def iterate_json_files_directory(dir,
     print('Reading {}'.format(dir))
     print('\tColumns: {}'.format(', '.join(columns_to_keep if columns_to_keep is not None else 'ALL')))
     for filepath in glob(os.path.join(dir, '*.json')):
-        print('\tReading {}'.format(filepath))
         for datum in iterate_json_data(filepath,
                                        columns_to_keep=columns_to_keep,
                                        feature_adder=feature_adder,
