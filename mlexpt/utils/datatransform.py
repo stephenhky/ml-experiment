45374def90606daa34c2f789a0e98c7511c7855d
stephenhky <stephenhky@yahoo.com.hk>
2020-05-02 16:27:02 -0400

    print out

--- mlexpt/utils/datatransform.py
+++ mlexpt/utils/datatransform.py
@@ -270,4 +270,4 @@ class PreparingCachedNumericallyPreparedDataset(CachedNumericallyPreparedDataset
         for i in range(Y.shape[1]):
             df[ycolumns[i]] = Y.toarray()[:, i]
         df.to_hdf(os.path.join(self.h5dir, filename), key=os.path.basename(filename)[:-3])
-
+        print('wrote {}'.format(filename))
