6aa9ced627f2fe478d9f70d0a92f1b7ef5ed9c76
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2020-06-24 22:20:34 -0400

    release 0.0.6

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def package_description():
 
 
 setup(name='ml-experiment',
-      version="0.0.6a01",
+      version="0.0.6",
       description="Machine Learning Experiment Framework",
       long_description=package_description(),
       long_description_content_type='text/markdown',
