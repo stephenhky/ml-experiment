5415ad23cb2fe077265546b686496d633d416fbb
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2024-09-27 11:06:54 -0400

    name change due to US naturalization

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 # General information about the project.
 project = 'ml-experiment'
 copyright = '2020, Kwan Yuet Stephen Ho'
-author = 'Kwan-Yuet Ho'
+author = 'Kwan Yuet Stephen Ho'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
